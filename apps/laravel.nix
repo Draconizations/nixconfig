168e3b4f659f9bfefad4cc25ba08e217fbf62921
Jake Fulmine <jake@fulmine.xyz>
2025-05-22 08:51:05 +0200

    maybe i understand it now

--- apps/laravel.nix
+++ apps/laravel.nix
@@ -118,12 +118,15 @@ in
   );
 
   config.systemd.services = lib.mkMerge [
-    map (app: lib.nameValuePair ("phpfpm-" + app.name)
-    (if config.flxmine.caddy.enable then forceHome app else {}))
+    if config.fxlmine.caddy.enable then builtins.listToAttrs (
+      map (app: lib.nameValuePair ("phpfpm-" + app.name)
+        (if config.flxmine.caddy.enable then forceHome app else {}))
       config.fxlmine.laravelApps
-    map (app: lib.nameValuePair ("backup-" + app.name)
-      (if app.backup != null then mkService app else {}))
-    config.fxlmine.laravelApps
+    ) else {};
+    builtins.listToAttrs (
+      map (app: lib.nameValuePair ("backup-" + app.name)
+        (if app.backup != null then mkService app else {}))
+      config.fxlmine.laravelApps);
   ];
   
   config.services.caddy.virtualHosts = if config.fxlmine.caddy.enable then builtins.listToAttrs (
